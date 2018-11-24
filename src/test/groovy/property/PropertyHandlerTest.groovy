9aa5e452bbd7b1b2814d1d8698ac90d50e710589
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-25 00:59:38 +0100

    PropertyHandlerTest.groovy tests

--- src/test/groovy/property/PropertyHandlerTest.groovy
+++ src/test/groovy/property/PropertyHandlerTest.groovy
@@ -16,15 +16,25 @@ class PropertyHandlerTest extends Specification {
 //        then:
 //        // TODO implement assertions
 //    }
-//
-//    def "test get"() {
-//        given:
-//
-//        when:
-//        // TODO implement stimulus
-//        then:
-//        // TODO implement assertions
-//    }
+
+    def "test get - key exists"() {
+        given:
+        def propertyHandler = new PropertyHandler(ImmutableMap.of(
+                "user.name", "michal"
+        ))
+        
+        expect:
+        propertyHandler.get("user.name") == Optional.of("michal")
+    }
+
+    def "test get - does not exist"() {
+        given:
+        def propertyHandler = new PropertyHandler(ImmutableMap.of(
+        ))
+
+        expect:
+        propertyHandler.get("user.name") == Optional.empty()
+    }
 
     def "test allPropertiesFor"() {
         given:
@@ -35,7 +45,7 @@ class PropertyHandlerTest extends Specification {
         ))
 
         when:
-        def properties = propertyHandler.propertiesOf(ImmutableList.of("user.name", "user.surname", "user.login"))
+        def properties = propertyHandler.propertiesOf(ImmutableList.of("user.name", "user.surname", "user.login", "NOT-EXISTS"))
 
         then:
         properties == "michal,tumilowicz,mtumilowicz"
