c52d1b137fedc169d25c126709b86d0eeaae6936
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-25 00:49:56 +0100

    yearTagsExpensesMap tests

--- src/test/groovy/customer/CustomerTest.groovy
+++ src/test/groovy/customer/CustomerTest.groovy
@@ -1,8 +1,13 @@
 package customer
 
 import com.google.common.collect.ImmutableList
+import com.google.common.collect.ImmutableMap
+import com.google.common.collect.ImmutableSet
+import expense.Expense
 import org.apache.commons.lang3.tuple.ImmutableTriple
 import spock.lang.Specification
+
+import java.time.Year
 
 /**
  * Created by mtumilowicz on 2018-11-24.
@@ -87,7 +92,7 @@ class CustomerTest extends Specification {
 
         when:
         def maxOrderOptional = customer.findOrderWithMaxPrice()
-        
+
         then:
         maxOrderOptional == Optional.ofNullable(orders.findAll { it.hasPrice() }.max { it.price })
     }
@@ -104,7 +109,7 @@ class CustomerTest extends Specification {
         then:
         maxOrderOptional == Optional.empty()
     }
-    
+
     def "findTop3OrdersByPrice empty order list"() {
         given:
         def customer = Customer.builder()
@@ -112,7 +117,7 @@ class CustomerTest extends Specification {
                 .build()
 
         expect:
-        
+
         customer.findTop3OrdersByPrice() == ImmutableTriple.nullTriple()
     }
 
@@ -238,5 +243,50 @@ class CustomerTest extends Specification {
         top3.left.id == 3
         top3.middle.id == 4
         top3.right.id == 1
+    }
+
+    def "yearTagsExpensesMap null expenses"() {
+        given:
+        def customer = Customer.builder().build()
+
+        expect:
+        customer.yearTagsExpensesMap() == ImmutableMap.of()
+    }
+
+    def "yearTagsExpensesMap - two different years, many different and repeatable tags"() {
+        given:
+        def expenses = ImmutableList.of(
+                Expense.builder()
+                        .year(Year.of(1990))
+                        .tags(ImmutableSet.of("travel", "entertainment", "food",
+                        "car", "gasoline"))
+                        .build(),
+                Expense.builder()
+                        .year(Year.of(1990))
+                        .tags(ImmutableSet.of("food", "entertainment", "cinema"))
+                        .build(),
+                Expense.builder()
+                        .year(Year.of(2000))
+                        .tags(ImmutableSet.of("clothes", "electronics"))
+                        .build()
+        )
+
+        and:
+        def customer = Customer.builder()
+                .expenses(expenses)
+                .build()
+
+        when:
+        def yearTagsExpensesMap = customer.yearTagsExpensesMap()
+
+        then:
+        yearTagsExpensesMap[Year.of(1990)] == ["entertainment",
+                                               "car",
+                                               "gasoline",
+                                               "cinema",
+                                               "travel",
+                                               "food"] as Set
+        yearTagsExpensesMap[Year.of(2000)] == ["clothes",
+                                               "electronics"] as Set
     }
 }
