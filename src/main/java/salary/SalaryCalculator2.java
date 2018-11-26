f47c8e483f88725801e1536c31e5b33f84657260
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-11-26 22:42:56 +0100

    SalaryCalculator2.java - composing instead of aggregation

---
+++ src/main/java/salary/SalaryCalculator2.java
@@ -0,0 +1,27 @@
+package salary;
+
+import java.util.function.DoubleUnaryOperator;
+
+/**
+ * Created by mtumilowicz on 2018-11-26.
+ */
+public class SalaryCalculator2 {
+    private final DoubleUnaryOperator operator;
+
+    public SalaryCalculator2() {
+        this(x -> x);
+    }
+    
+    private SalaryCalculator2(DoubleUnaryOperator operator) {
+        this.operator = operator;
+    }
+
+    public SalaryCalculator2 with(SalaryRules rule) {
+        return new SalaryCalculator2(operator.andThen(rule.operator));
+    }
+
+    public double calculate(double salary) {
+        return operator.applyAsDouble(salary);
+
+    }
+}
