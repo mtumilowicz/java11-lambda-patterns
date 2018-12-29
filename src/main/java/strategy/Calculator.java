def1f0c9cb7e16e35eb75ec291b825f9cd158ef9
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-12-30 00:47:18 +0100

    calculator refactoring (real life example)

--- src/main/java/strategy/Calculator.java
+++ src/main/java/strategy/Calculator.java
@@ -1,30 +1,50 @@
 package strategy;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.Value;
+
 import java.util.List;
 import java.util.function.IntPredicate;
+import java.util.function.IntUnaryOperator;
 
 /**
  * Created by mtumilowicz on 2018-11-30.
  */
 
-/*
- * change to stocks: stream: stream().map(i -> getPrice(i)).filter(predicate -> PriceLessThan).sum()
- */
-final class Calculator {
-    static int totalValues(List<Integer> integers, IntPredicate take) {
+@Value
+class Calculator {
+    PriceProvider priceProvider;
+
+    int totalValues(List<Stock> integers, IntPredicate take) {
         return integers.stream()
-                .mapToInt(i -> i)
+                .map(Stock::getId)
+                .mapToInt(priceProvider::getPrice)
                 .filter(take)
                 .sum();
     }
 
 
-    // library of functions
-    static boolean isEven(int number) {
-        return number % 2 == 0;
+    static IntPredicate priceLessThan(int limit) {
+        return it -> it < limit;
     }
 
-    static boolean isOdd(int number) {
-        return number % 2 != 0;
+    static IntPredicate priceEquals(int limit) {
+        return it -> it == limit;
+    }
+}
+
+@Value
+class Stock {
+    int id;
+}
+
+@Value
+class PriceProvider {
+    @Getter(AccessLevel.NONE)
+    IntUnaryOperator priceSource;
+
+    int getPrice(int id) {
+        return priceSource.applyAsInt(id);
     }
 }
--- src/test/groovy/strategy/CalculatorTest.groovy
+++ src/test/groovy/strategy/CalculatorTest.groovy
@@ -2,32 +2,48 @@ package strategy
 
 import spock.lang.Specification
 
-import static strategy.Calculator.*
+import java.util.function.IntUnaryOperator
 
 /**
  * Created by mtumilowicz on 2018-11-30.
  */
 class CalculatorTest extends Specification {
-    
-    def "test totalValues even"() {
+
+    def "test totalValues < 4"() {
         given:
-        def integers = [1, 2, 3, 4, 5, 6, 7]
+        def stocks = [new Stock(1),
+                      new Stock(2),
+                      new Stock(3),
+                      new Stock(4),
+                      new Stock(5),
+                      new Stock(6),
+                      new Stock(7)]
+        
+        def calculator = new Calculator(new PriceProvider(IntUnaryOperator.identity()))
 
         when:
-        def sum = Calculator.totalValues(integers, { isEven(it)})
+        def sum = calculator.totalValues(stocks, Calculator.priceLessThan(4))
 
         then:
-        sum == 12
+        sum == 6
     }
 
-    def "test totalValues odd"() {
+    def "test totalValues <= 5"() {
         given:
-        def integers = [1, 2, 3, 4, 5, 6, 7]
+        def stocks = [new Stock(1),
+                      new Stock(2),
+                      new Stock(3),
+                      new Stock(4),
+                      new Stock(5),
+                      new Stock(6),
+                      new Stock(7)]
+
+        def calculator = new Calculator(new PriceProvider(IntUnaryOperator.identity()))
 
         when:
-        def sum = Calculator.totalValues(integers, { isOdd(it)})
+        def sum = calculator.totalValues(stocks, Calculator.priceLessThan(3) | Calculator.priceEquals(5))
 
         then:
-        sum == 16
+        sum == 8
     }
 }
