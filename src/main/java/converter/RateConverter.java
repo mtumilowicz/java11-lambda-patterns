ca64dd89963cbca3f6c60bf1de331a33941268b3
mtumilowicz <michal.tumilowicz01@gmail.com>
2018-12-30 15:36:40 +0100

    RateConverter.java -> public

--- src/main/java/converter/RateConverter.java
+++ src/main/java/converter/RateConverter.java
@@ -5,7 +5,7 @@ import java.util.function.DoubleUnaryOperator;
 /**
  * Created by mtumilowicz on 2018-11-26.
  */
-class RateConverter implements CurrableDoubleBinaryOperator {
+public class RateConverter implements CurrableDoubleBinaryOperator {
 
     @Override
     public double applyAsDouble(double value, double rate) {
