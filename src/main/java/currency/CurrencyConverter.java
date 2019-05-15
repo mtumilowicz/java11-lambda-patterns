ef532fb31e7c4bb97670942760170102247143cb
mtumilowicz <michal.tumilowicz01@gmail.com>
2019-05-15 20:06:23 +0200

    CurrencyConverter.java draft

---
+++ src/main/java/currency/CurrencyConverter.java
@@ -0,0 +1,71 @@
+package currency;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Created by mtumilowicz on 2019-05-15.
+ * <p>
+ * TO-DO
+ */
+public interface CurrencyConverter {
+
+    BigDecimal convert(BigDecimal amount);
+
+    interface BiFunction {
+        BigDecimal convert(BigDecimal amount, String toCurrency);
+
+        default CurrencyConverter to(String toCurrency) {
+            return amount -> convert(amount, toCurrency);
+        }
+    }
+
+    interface TriFunction {
+        BigDecimal convert(BigDecimal amount, String fromCurrency, String toCurrency);
+
+        default BiFunction from(String fromCurrency) {
+            return (amount, toCurrency) -> convert(amount, fromCurrency, toCurrency);
+        }
+    }
+
+    static TriFunction of(LocalDate date) {
+
+        return (amount, fromCurrency, toCurrency) -> {
+
+            Path path = Paths.get("src/main/resources/currency.txt");
+            try (Stream<String> lines = Files.lines(path)) {
+
+                Map<String, BigDecimal> converterMap =
+                        lines.skip(1L)
+                                .collect(
+                                        Collectors.toMap(
+                                                line -> line.substring(0, 3),
+                                                line -> new BigDecimal(line.substring(4))
+                                        )
+                                );
+
+                return amount.multiply(converterMap.get(toCurrency).divide(converterMap.get(fromCurrency),
+                        RoundingMode.DOWN));
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        };
+    }
+
+    static void main(String[] args) {
+        System.out.println(CurrencyConverter.of(LocalDate.of(2018, 11, 5))
+                .from("EUR")
+                .to("GBP").convert(BigDecimal.valueOf(1000)));
+    }
+}
